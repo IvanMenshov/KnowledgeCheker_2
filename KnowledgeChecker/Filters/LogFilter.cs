d9abce49cfaa8cbe182b4b4433c46b78af025591
IvanMenshov <ivan.menshou@gmail.com>
2019-08-22 16:23:20 +0300

    Add project files.

---
+++ KnowledgeChecker/Filters/LogFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+
+namespace KnowledgeChecker.Filters
+{
+    public class LogFilter : ActionFilterAttribute
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var user = (filterContext.Controller as Controller)?.User as ClaimsPrincipal;
+            var id = user.Claims.FirstOrDefault(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+
+            if (id != null)
+            {
+                logger.SetProperty("controller", filterContext.ActionDescriptor.ControllerDescriptor.ControllerName);
+                logger.SetProperty("action", filterContext.ActionDescriptor.ActionName);
+                logger.Info(Environment.NewLine + DateTime.Now);
+                logger.SetProperty("UserId", id);
+            }
+        }
+    }
+}
---
+++ KnowledgeChecker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnowledgeChecker.Startup))]
+namespace KnowledgeChecker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
